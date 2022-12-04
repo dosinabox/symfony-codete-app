ab605e012494d21c8ab947ef6bb9874965634d3a
dosinabox <39790055+dosinabox@users.noreply.github.com>
2022-12-04 16:00:15 +0100

    add mailhog and mailer

--- src/Application/Command/DeleteUserCommandHandler.php
+++ src/Application/Command/DeleteUserCommandHandler.php
@@ -4,13 +4,16 @@ namespace App\Application\Command;
 
 use App\Application\Query\GetUserByIDQuery;
 use App\Application\Query\GetUserByIDQueryHandler;
+use App\Event\UserDeletedEvent;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
 
 class DeleteUserCommandHandler
 {
     public function __construct(
         private readonly GetUserByIDQueryHandler $queryHandler,
-        private readonly EntityManagerInterface $entityManager)
+        private readonly EntityManagerInterface $entityManager,
+        private readonly EventDispatcherInterface $dispatcher)
     {
     }
 
@@ -20,5 +23,7 @@ class DeleteUserCommandHandler
 
         $this->entityManager->remove($user);
         $this->entityManager->flush();
+
+        $this->dispatcher->dispatch(new UserDeletedEvent($user), UserDeletedEvent::NAME);
     }
 }
