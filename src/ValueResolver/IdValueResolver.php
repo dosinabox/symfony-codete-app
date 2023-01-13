968683b8a8969e029df71e93e0677595090453b7
dosinabox <39790055+dosinabox@users.noreply.github.com>
2023-01-13 14:38:06 +0100

    fix resolver

--- src/ValueResolver/IdValueResolver.php
+++ src/ValueResolver/IdValueResolver.php
@@ -5,17 +5,22 @@ namespace App\ValueResolver;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Uid\Uuid;
 
 class IdValueResolver implements ArgumentValueResolverInterface
 {
     public function supports(Request $request, ArgumentMetadata $argument): bool
     {
-        //return $argument->getType() === 'string';
-        return true;
+        return $argument->getType() === Uuid::class && $request->attributes->has('id');
     }
 
-    public function resolve(Request $request, ArgumentMetadata $argument): array
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
-        return [$request->attributes->get($argument->getName())];
+        try {
+            yield Uuid::fromString($request->attributes->get('id'));
+        } catch (\InvalidArgumentException $exception) {
+            throw new BadRequestHttpException($exception->getMessage());
+        }
     }
 }
